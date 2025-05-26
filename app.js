3287b1691214784dc02d245f7bc5495b76a593b1
kaique2008KA <kaique.schmitz.santos@escola.pr.gov.br>
2025-05-26 10:56:51 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,28 @@
-function criaCartao(categoria, pergunta, resposta) {
-  let container= document.getElementByld('container')
-  let cartao = document.crateElement('article')
-  cartao.className = 'cartao'
+function criaCartao(categoria,pergunta,resposta){
+  let container=document.getElementByld('container')
+  let Cartao=document.createElement('article')
+  Cartao.className='Cartao'
+
+   caetao.innHTML=
+   <div class="cartao__conteudo">
+   <h3>$(categoria)</h3>
+   <div class="cartao__conteudo__pergunta">
+     <p>${pergunta}</p>
+     </div>
+  <div class="cartao__conteudo__resposta">
+     <p>${resposta</p>
+     </div>
+     </div>
+
+  funcion viraCartao(){
+     let respostaEstavisivel=false
+     function viraVisivel=!respostaEstaVisivel
+     cartao.classList.toggle('activ'resposstaEstaVisivel
+  }
+  cartao.addEventListener('click',vraCartao)
+
+  container.appendChild(cartao)
+  }
+
+
   
