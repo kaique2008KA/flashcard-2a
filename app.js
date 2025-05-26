6b11dc5534d8620c795538f9bb6e1d108ddfadfa
kaique2008KA <kaique.schmitz.santos@escola.pr.gov.br>
2025-05-26 10:23:25 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,5 @@
-
+function criaCartao(categoria, pergunta, resposta) {
+  let container= document.getElementByld('container')
+  let cartao = document.crateElement('article')
+  cartao.className = 'cartao'
+  
